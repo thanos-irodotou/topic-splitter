687cdc5de0316066ccf55d97df141f5b7df7b52c
Thanos Irodotou <athanasios@brandwatch.com>
2016-10-21 10:42:42 +0100

    Drop

---
+++ src/main/java/com/brandwatch/TopicSplitterApplication.java
@@ -0,0 +1,12 @@
+package com.brandwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicSplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicSplitterApplication.class, args);
+	}
+}
